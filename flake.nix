6012207a0bf6394cd67cc1e509a6e89d057dcc1c
Jamie Bowers <james.t.k.bowers@gmail.com>
2023-07-24 12:03:14 +1200

    Add basic dev shell with placeholder cargo command.

--- flake.nix
+++ flake.nix
@@ -1,11 +1,61 @@
 {
   description = "A very basic flake";
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nix-bubblewrap = {
+      url = "github:JTKBowers/nix-bubblewrap";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    nix-bubblewrap,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [rust-overlay.overlays.default];
+        };
+        cargo = nix-bubblewrap.lib.wrapPackage pkgs {
+          pkg = pkgs.rust-bin.stable.latest.default;
+          name = "cargo";
+          bindCwd = "ro";
+          envs = {
+            "HOME" = "$HOME";
+          };
+          shareNet = true;
+          extraArgs = [
+            "--proc /proc"
+          ];
+          extraRoBindDirs = [
+            "$WAYLAND_DISPLAY"
+          ];
+        };
+      in {
+        packages = {
+          hello = pkgs.hello;
+          default = pkgs.hello;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.openssl
+            pkgs.pkg-config
+            cargo
+          ];
+        };
+      }
+    );
 }
